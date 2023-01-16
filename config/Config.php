2966d878cd3f71a53e32ff048886149a8fb06523
cxfunkyman <92549556+cxfunkyman@users.noreply.github.com>
2023-01-16 10:37:42 -0500

    Completed

---
+++ config/Config.php
@@ -0,0 +1,11 @@
+<?php
+define('BASE_URL', 'http://localhost/innovatank-machines/EssaySpellChecker/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'dictionary');
+//define('DBNAME', 'spellcheck');
+define('CHARSET', 'utf8');
+define('TITLE', 'SPELL CHECK TEST');
+define('CURRENCY', '$/ ');
+?>
